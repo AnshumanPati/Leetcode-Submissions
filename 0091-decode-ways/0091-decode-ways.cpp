7031ef1e2f4afdae9f170677f5e4cf7c8d325b30
Anshuman Pati <anshpati23@gmail.com>
2023-03-02 05:54:12 +0530

    Time: 5 ms (26.51%), Space: 6.3 MB (52.65%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,22 @@
+class Solution {
+
+public:
+    int numDecodings(string s) {
+        int n = s.length();
+        if (n == 0) return 0;
+        vector<int> dp(n+1, 0);
+        dp[0] = 1;
+        dp[1] = (s[0] == '0') ? 0 : 1;
+        for (int i = 2; i <= n; i++) {
+            int one_digit = stoi(s.substr(i-1, 1));
+            int two_digits = stoi(s.substr(i-2, 2));
+            if (one_digit >= 1) {
+                dp[i] += dp[i-1];
+            }
+            if (two_digits >= 10 && two_digits <= 26) {
+                dp[i] += dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+};
